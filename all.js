88b96dfc902d1adef256611fe9c7f1cf1529cda2
sky0929870211 <sky0929870211@gmail.com>
2024-01-03 15:34:44 +0800

    ad

--- all.js
+++ all.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
-    $('h1').hide();
-    $('.button').click(function (event) { 
-        $('h1').show();
+    //$('h1').hide();
+    //$('.button').click(function (event) { 
+     //   $('h1').show();
         
+    //});
+    //$('title').hide();
+    //$('h1').show
+    $('.button').click(function(){
+        $('h1').hide();
     });
+    $('.button').click(function(event){
+        $('p').toggle();
+    });
+    
 });
